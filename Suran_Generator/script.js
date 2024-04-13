0a202ff837872822a4dc8d667fdaf1a452ef8f70
Billy-Ray-Valentine-83 <83879349+Billy-Ray-Valentine-83@users.noreply.github.com>
2024-04-12 20:57:32 -0400

    added a new project name Quran_search

--- Suran_Generator/script.js
+++ Suran_Generator/script.js
@@ -5,6 +5,26 @@ const btn = document.getElementById('btn');
 
 
 
+/*  THE ASYNC FUNCTION
+async function getSura() {
+    
+    x = Math.floor(Math.random() * 226)
+    suraContainer.classList.remove('fade')
+    res = await fetch(`http://api.alquran.cloud/v1/ayah/${x}/en.asad`)
+    data = await res. json()
+    .then(data => {
+        suraContainer.textContent = `${data.data.text}`;
+        suraContainer1.textContent = `${data.data.numberInSurah}`;
+        suraContainer2.textContent = `${data.data.surah.englishName}`
+        suraContainer.classList.add("fade");  
+        console.log(data)
+        console.log(data.data.numberInSurah)
+    });
+ */
+
+
+
+
 function getSura() {
     
     x = Math.floor(Math.random() * 226)
